2405a40497173d6068798749b9d6c4ef3d4aa51a
SystemAgainst <87114257+SystemAgainst@users.noreply.github.com>
2021-10-19 14:03:13 +0300

    The parser scrab datas such as price, link

---
+++ CsharpParser/CsharpParser/FormParser.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace CsharpParser
+{
+    partial class FormParser
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.btnParsing = new System.Windows.Forms.Button();
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(12, 23);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxResult.Size = new System.Drawing.Size(490, 399);
+            this.textBoxResult.TabIndex = 0;
+            // 
+            // btnParsing
+            // 
+            this.btnParsing.Location = new System.Drawing.Point(523, 329);
+            this.btnParsing.Name = "btnParsing";
+            this.btnParsing.Size = new System.Drawing.Size(184, 36);
+            this.btnParsing.TabIndex = 1;
+            this.btnParsing.Text = "Парсить";
+            this.btnParsing.UseVisualStyleBackColor = true;
+            this.btnParsing.Click += new System.EventHandler(this.btnParsing_Click);
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.Location = new System.Drawing.Point(523, 386);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(184, 36);
+            this.btnRemoveAll.TabIndex = 2;
+            this.btnRemoveAll.Text = "Очистить";
+            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.btnRemoveAll.Click += new System.EventHandler(this.btnRemoveAll_Click);
+            // 
+            // FormParser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.btnRemoveAll);
+            this.Controls.Add(this.btnParsing);
+            this.Controls.Add(this.textBoxResult);
+            this.Name = "FormParser";
+            this.Text = "Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button btnParsing;
+        private System.Windows.Forms.Button btnRemoveAll;
+    }
+}
+
---
+++ CsharpParser/CsharpParser/FormParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+
+namespace CsharpParser
+{
+    public partial class FormParser : Form
+    {
+        public FormParser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParsing_Click(object sender, EventArgs e)
+        {
+            GetHtmlAsync();
+        }
+       
+        private async void GetHtmlAsync()
+        {
+            var url = "https://www.avito.ru/krasnodar/avtomobili/toyota/mark_ii-ASgBAgICAkTgtg20mSjitg2qqig?f=ASgBAgECA0TyCrCKAeC2DbSZKOK2DaqqKAFFxpoMFnsiZnJvbSI6MCwidG8iOjMwMDAwMH0&radius=200";
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var productsHtml = htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("items-items-kAJAg")).ToList();
+
+            var productListItems = productsHtml[0].Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Contains("iva-item-root-Nj_hb")).ToList();
+
+            foreach (var productListItem in productListItems)
+            {
+                // id предмета
+                textBoxResult.Text += "Id предмета: " + productListItem.GetAttributeValue("data-item-id", "") + "\r\n";
+
+                // заголовок.
+                textBoxResult.Text += "Заголовок автомобиля: " + productListItem.Descendants("h3")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("title-root-j7cja iva-item-title-_qCwt title-listRedesign-XHq38 title-root_maxHeight-SXHes text-text-LurtD text-size-s-BxGpL text-bold-SinUO"))
+                    .FirstOrDefault().InnerText + "\r\n";
+
+                // стоимость
+                textBoxResult.Text += "Стоимость автомобиля: " + productListItem.Descendants("span")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("price-text-E1Y7h text-text-LurtD text-size-s-BxGpL"))
+                .FirstOrDefault().InnerText + "\r\n";
+
+                // url
+                textBoxResult.Text += "Url ссылка на автомобиль: https://www.avito.ru" + productListItem.Descendants("a").FirstOrDefault().GetAttributeValue("href", "") + "\r\n" + "\r\n";
+            }
+        }
+
+        private void btnRemoveAll_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = "";
+        }
+    }
+}
